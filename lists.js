363aabb6cc0d0b077f89c8939209214ad6b2b5d2
Henry Ripple <hripple46@gmail.com>
2022-12-26 17:15:59 -0600

    added function size that returns the length of linked lsit

--- lists.js
+++ lists.js
@@ -19,13 +19,21 @@ function linkedList() {
     newNode.next = HEAD.next;
     HEAD.next = newNode;
   }
-  return { HEAD, addNode, prepend };
+  function size() {
+    let i = 0;
+    let TAIL = list.HEAD;
+
+    while (TAIL.next !== null) {
+      i += 1;
+      TAIL = TAIL.next;
+    }
+    console.log(i);
+  }
+  return { HEAD, addNode, prepend, size };
 }
 let list = linkedList();
 list.addNode("1");
 list.addNode("2");
 list.prepend("-7");
-
 list.addNode("3");
-//list.addNode("2");
-console.log(list);
+list.size();
